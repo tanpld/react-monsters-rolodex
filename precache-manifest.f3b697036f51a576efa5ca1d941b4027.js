c3cdf66f160e9db952ec98942a99998e7ad5b860
Tan Phan <tan.phan@alley51.com>
2019-07-10 04:51:46 +0700

    Updates

---
+++ precache-manifest.f3b697036f51a576efa5ca1d941b4027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8272b2f4516e5723f26ac6367d68e56e",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4254bdbb4d2c79814b57",
+    "url": "/react-monsters-rolodex/static/css/main.c24e8fc0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4254bdbb4d2c79814b57",
+    "url": "/react-monsters-rolodex/static/js/main.4a6a1daf.chunk.js"
+  },
+  {
+    "revision": "09fc101723e82bb4a8f4",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.2a38a34c.js"
+  }
+]);
